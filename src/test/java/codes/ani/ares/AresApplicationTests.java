658f972157504a5a03fa8140f5989d885e5e5d38
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2026-04-18 11:42:02 +0530

    init: initialize Ares application with Gradle setup and basic configuration files

---
+++ src/test/java/codes/ani/ares/AresApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.ani.ares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
