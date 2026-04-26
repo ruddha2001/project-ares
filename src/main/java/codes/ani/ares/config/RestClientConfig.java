292821561cb11eec58ef33296dd2127df02e5aa2
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2026-04-26 12:54:50 +0530

    feat(config): enable virtual threads in RestClient configuration

--- src/main/java/codes/ani/ares/config/RestClientConfig.java
+++ src/main/java/codes/ani/ares/config/RestClientConfig.java
@@ -2,12 +2,16 @@ package codes.ani.ares.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.net.http.HttpClient;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class RestClientConfig {
     @Bean
     public RestClient restClient(RestClient.Builder builder) {
-        return builder.build();
+        return builder.requestFactory(new JdkClientHttpRequestFactory(HttpClient.newBuilder().executor(Executors.newVirtualThreadPerTaskExecutor()).build())).build();
     }
 }
