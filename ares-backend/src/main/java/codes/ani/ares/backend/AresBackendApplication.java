67af765fc611fd684b3f308200554677373d2876
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2026-06-06 16:59:32 +0530

    init (ares-backend): initialize project structure

---
+++ ares-backend/src/main/java/codes/ani/ares/backend/AresBackendApplication.java
@@ -0,0 +1,13 @@
+package codes.ani.ares.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AresBackendApplication.class, args);
+	}
+
+}
