f22af195c21950f5f84c5c6081f4649ac4f02b4f
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2026-06-11 22:18:04 +0530

    feat: add @NoArgsConstructor and @AllArgsConstructor to Project, AresJob, and KnowledgeIndex models

--- ares-backend/src/main/java/codes/ani/ares/backend/model/Project.java
+++ ares-backend/src/main/java/codes/ani/ares/backend/model/Project.java
@@ -1,14 +1,18 @@
 package codes.ani.ares.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "ares_projects")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Project {
     @Id
